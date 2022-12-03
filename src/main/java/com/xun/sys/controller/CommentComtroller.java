7249b8dc30ba04f08038dcede4d4804c85c644cf
xun <3296497389@qq.com>
2022-12-04 02:28:50 +0800

    评论管理

--- src/main/java/com/xun/sys/controller/CommentComtroller.java
+++ src/main/java/com/xun/sys/controller/CommentComtroller.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.Map;
+
 /**
  * @version: java version 1.8
  * @Author: xun
@@ -23,5 +26,37 @@ public class CommentComtroller {
     @RequestMapping ( "findComment" )
     public JsonResult findComment ( CommentVO comment, @RequestParam ( required = false, defaultValue = "1" ) Integer curPage, @RequestParam ( required = false, defaultValue = "5" ) Integer pageSize ) {
         return commentService.findComment ( comment, curPage, pageSize );
+    }
+
+    /**
+     * 审核
+     *
+     * @param id
+     * @param commentState
+     * @return
+     */
+    @RequestMapping ( "updateCommentState" )
+    public JsonResult updateCommentState ( Integer id, Integer commentState ) {
+        Integer n = commentService.updateCommentState ( id, commentState );
+        JsonResult jr = new JsonResult ( n );
+        jr.setMsg ( "修改成功！" );
+        return jr;
+    }
+
+    /**
+     * 删除
+     */
+    @RequestMapping ( "deleteComment" )
+    public JsonResult deleteComment ( @RequestParam ( "ids[]" ) Integer[] ids ) {
+        int n = commentService.deleteComment ( ids );
+        JsonResult jr = new JsonResult ( n );
+        jr.setMsg ( n + "条数据已删除！" );
+        return jr;
+    }
+
+    @RequestMapping ( "findObjectes" )
+    public JsonResult findObjectes ( ) {
+        List< Map< String, Object > > objects = commentService.findObjects ( );
+        return new JsonResult ( objects );
     }
 }
